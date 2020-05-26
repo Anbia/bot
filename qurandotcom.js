33080b83d83cd62e9d940c661fc00c8a8692f9cc
Anbia <33218189+Anbia@users.noreply.github.com>
2020-05-26 19:01:14 +0700

    Update qurandotcom.js

--- qurandotcom.js
+++ qurandotcom.js
@@ -8,3 +8,4 @@ jQuery('footer').hide();
 jQuery('.montserrat').remove(); // hapus translate bahasa
 jQuery('._3Wvc2PyRmdtcwk1ww7wZ_A').closest('div').remove(); // hapus referensi youtube
 jQuery('._1HRwXM7KcdMYT7QJClh73-').hide(); // hapus button di kiri layar
+jQuery('._36RmFyvbszTZpOnHgHKSY0').hide(); // hide button sosial media
